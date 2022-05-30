91db1053f10d82796b7a16297778266235991784
Ayaan Jaleel <ayaanjaleel@dbz.local>
2022-05-30 16:01:27 +0400

    creating async function to fetch stock data

--- stock-api.js
+++ stock-api.js
@@ -1,8 +1,13 @@
 import fetch from 'node-fetch';
 
-const response = await fetch('https://api.github.com/users/github');
-const data = await response.json();
+async function get_request(){
+    const url = 'https://jsonmock.hackerrank.com/api/stocks?date=5-January-2001&fields=open,high,close,low,date,rise';
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+}
 
-console.log(data);
+console.log(get_request())
+
 
 
