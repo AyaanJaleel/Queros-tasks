c2c80877b5cabe007d8bf5779fa72e1e18a98a6e
Ayaan Jaleel <ayaanjaleel@dbz.local>
2022-05-30 16:19:09 +0400

    creating function to handle stock data

--- stock-api.js
+++ stock-api.js
@@ -1,12 +1,27 @@
 import fetch from 'node-fetch';
 
+function handleData(stock){
+
+    if(stock.length === 0 || stock === null){
+        return 'There is no stock data to handle.'
+    }
+    
+    console.log('Input: '+ stock.date)
+    console.log('Output:')
+    console.log('Open: '+ stock.open)
+    console.log('High: '+ stock.high)
+    console.log('Low: '+ stock.low)
+    console.log('Close: '+ stock.close)
+}
+
 async function get_request(){
     const url = 'https://jsonmock.hackerrank.com/api/stocks?date=5-January-2001&fields=open,high,close,low,date,rise';
     fetch(url).then(response=> response.json()).then(data=>{
-        console.log(data);
-        }).catch(error=>{
-            return error;
-        });
+        let stock_data = data['data'][0]
+        handleData(stock_data);
+    }).catch(error=>{
+        return error;
+    });
 }
 
 console.log(get_request())
