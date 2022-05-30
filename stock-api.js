cd2fa3ad2ddc47997b2459fb194f4207d22f6b33
Ayaan Jaleel <ayaanjaleel@dbz.local>
2022-05-30 16:08:16 +0400

    handling fetch errors

--- stock-api.js
+++ stock-api.js
@@ -2,9 +2,11 @@ import fetch from 'node-fetch';
 
 async function get_request(){
     const url = 'https://jsonmock.hackerrank.com/api/stocks?date=5-January-2001&fields=open,high,close,low,date,rise';
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data)
+    fetch(url).then(response=> response.json()).then(data=>{
+        console.log(data);
+        }).catch(error=>{
+            return error;
+        });
 }
 
 console.log(get_request())
